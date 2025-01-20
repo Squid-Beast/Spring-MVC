79e51ad8df1d6533f249f79fcc35dafb9aec833a
Lohith Kumar <lkneerukonda@gmail.com>
2025-01-20 15:57:40 -0600

    added validation

--- src/main/java/com/example/springmvc_jsp/controller/EmployeeController.java
+++ src/main/java/com/example/springmvc_jsp/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.example.springmvc_jsp.controller;
 import com.example.springmvc_jsp.entity.Employee;
 import com.example.springmvc_jsp.service.EmployeeService;
 
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -35,7 +38,10 @@ public class EmployeeController {
     }
 
     @PostMapping("/addEmployee")
-    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+    public String saveEmployee(@Valid @ModelAttribute("employee") Employee employee, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "employeeForm";
+        }
         employeeService.saveEmployee(employee);
         return "employeeConfirmation";
     }
--- src/main/java/com/example/springmvc_jsp/entity/Employee.java
+++ src/main/java/com/example/springmvc_jsp/entity/Employee.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,8 +18,12 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "Name is required")
     private String name;
+    @NotEmpty(message = "Designation is required")
     private String designation;
+    @NotEmpty(message = "Department is required")
     private String department;
+    @Email(message = "Provide a valid email address")
     private String email;
 }
